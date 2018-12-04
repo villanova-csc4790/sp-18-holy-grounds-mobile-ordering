80c60d57d4c342af36c6bc410a1f4c308aa327bd
etenzinger <43051993+etenzinger@users.noreply.github.com>
2018-12-04 14:41:22 -0500

    Update MatchaLatteInfo.js

--- MatchaLatteInfo.js
+++ MatchaLatteInfo.js
@@ -70,7 +70,7 @@ export default class MatchaLatteInfo extends Component {
     );
     return(
       <Container style={{backgroundColor: '#fdfdfd'}}>
-        <Navbar left={left} right={right} title={this.state.product.title} style={{ fontSize: 25, color: 'white'}}/>
+        <Navbar left={left} right={right} style={{ fontSize: 25, color: 'white'}}/>
         <Content>
             <Image source={require('./Hot Drinks/MatchaLatte.jpg')} style={styles.image} />
           <View style={{backgroundColor: '#fdfdfd', paddingTop: 10, paddingBottom: 10, paddingLeft: 12, paddingRight: 12, alignItems: 'center'}}>
@@ -133,10 +133,6 @@ export default class MatchaLatteInfo extends Component {
                 </Picker>
               </Col>
             </Grid>
-
-           
-           
-           
             <Grid>
               <Col>
                 <View style={{flex: 1, justifyContent: 'center'}}>
@@ -268,12 +264,14 @@ renderFlavor() {
     }
   addToCart() {
     var product = this.state.product;
+    var price = this.price();
     product['color'] = this.state.selectedColor;
     product['size'] = this.state.selectedSize;
     product['milk'] = this.state.selectedMilk;
     product['quantity'] = this.state.quantity;
     product['xEspresso'] = this.state.xEspresso;
     product['extraflavors'] = this.state.selectedextraflavor;
+    product['price'] = price;
     AsyncStorage.getItem("CART", (err, res) => {
       if(!res) AsyncStorage.setItem("CART",JSON.stringify([product]));
       else {
@@ -281,13 +279,6 @@ renderFlavor() {
         items.push(product);
         AsyncStorage.setItem("CART",JSON.stringify(items));
       }
-      /*Toast.show({
-        text: 'Product added to your cart !',
-        position: 'bottom',
-        type: 'success',
-        buttonText: 'Dismiss',
-        duration: 3000
-      });*/
     });
   }
   
