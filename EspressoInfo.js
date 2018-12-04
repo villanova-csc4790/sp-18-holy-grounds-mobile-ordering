570de8bfa56563423ad3fcd1f673beb9f8d9c544
etenzinger <43051993+etenzinger@users.noreply.github.com>
2018-12-04 14:29:32 -0500

    Update EspressoInfo.js

--- EspressoInfo.js
+++ EspressoInfo.js
@@ -42,7 +42,217 @@ export default class EspressoInfo extends Component {
       selectedSize: defSize,
     });
   }
+import React, { Component } from 'react';
+import { Image, Dimensions, TouchableWithoutFeedback, AsyncStorage, StyleSheet } from 'react-native';
+import { View, Container, Content, Button, Left, Right, Icon, Picker, Item, Grid, Col, Toast, Text as NBText } from 'native-base';
+import { Actions } from 'react-native-router-flux';
+import Carousel, { Pagination } from 'react-native-snap-carousel';
 
+// Our custom files and classes import
+import Colors from './Colors';
+import Text from './Text';
+import Navbar from './Navbar';
+import {default as ProductComponent} from './Product';
+
+export default class EspressoInfo extends Component {
+
+  static navigationOptions = ({ navigation}) => {
+		const params = navigation.state.params || {};
+		return {
+			header: null,
+		}
+	};
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: {},
+      activeSlide: 0,
+      quantity: 1,
+      price: '',
+      selectedSize: '',
+    };
+  }
+
+  componentWillMount() {
+    //get the product with id of this.props.product.id from your server
+    this.setState({product: dummyProduct});
+  }
+
+  componentDidMount() {
+    /* Select the default color and size (first ones) */
+    let defSize = this.state.product.sizes[0];
+    this.setState({
+      selectedSize: defSize,
+    });
+  }
+
+  render() {
+    var left = (
+      <Left style={{flex:1}}>
+        <Button onPress={() => this.props.navigation.navigate('HotMenuScreen')} transparent>
+          <Icon name='ios-arrow-back' />
+        </Button>
+      </Left>
+    );
+    var right = (
+      <Right style={{flex:1}}>
+       <Button onPress={() => this.props.navigation.navigate('CartScreen')} transparent>
+          <Icon name='ios-cart' />
+        </Button>
+      </Right>
+    );
+    return(
+      <Container style={{backgroundColor: '#fdfdfd'}}>
+        <Navbar left={left} right={right} style={{ fontSize: 25, color: 'white'}}/>
+        <Content>
+            <Image source={require('./Hot Drinks/Espresso.jpg')} style={styles.image} />
+          <View style={{backgroundColor: '#fdfdfd', paddingTop: 10, paddingBottom: 10, paddingLeft: 12, paddingRight: 12, alignItems: 'center'}}>
+            <Grid>
+              <Col>
+                  <Text style={{fontSize: 25, fontWeight: 'bold'}}>${this.price()}</Text>: null}
+              </Col>
+            </Grid>
+            <Grid style={{marginTop: 15}}>
+              <Col>
+                <View style={{flex: 1, justifyContent: 'center'}}>
+                  <Text>Size:</Text>
+                </View>
+              </Col>
+              <Col size={3}>
+                <Picker
+                  mode="dropdown"
+                  placeholder="Select a size"
+                  note={true}
+                  selectedValue={this.state.selectedSize}
+                  onValueChange={(size) => this.setState({selectedSize: size})}
+                >
+                  {this.renderSize()}
+                </Picker>
+              </Col>
+            </Grid>
+            <Grid>
+              <Col>
+                <View style={{flex: 1, justifyContent: 'center'}}>
+                  <Text>Quantity:</Text>
+                </View>
+              </Col>
+              <Col size={3}>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <Button style={{flex: 1}} icon light onPress={() => this.setState({quantity: this.state.quantity>1 ? this.state.quantity-1 : 1})} >
+                    <Icon name='ios-remove-outline' />
+                  </Button>
+                  <View style={{flex: 4, justifyContent: 'center', alignItems: 'center', paddingLeft: 15, paddingRight: 15}}>
+                    <Text style={{fontSize: 18}}>{this.state.quantity}</Text>
+                  </View>
+                  <Button style={{flex: 1}} icon light onPress={() => this.setState({quantity: this.state.quantity+1})}>
+                    <Icon name='ios-add' />
+                  </Button>
+                </View>
+              </Col>
+            </Grid>
+            <Grid style={{marginTop: 15}}>
+              <Col size={3}>
+                <Button block onPress={this.addToCart.bind(this)} style={{backgroundColor: Colors.navbarBackgroundColor}}>
+                  <Text style={{color: "#fdfdfd", marginLeft: 5}}>Add to cart</Text>
+                </Button>
+              </Col>
+            </Grid>
+          </View>
+        </Content>
+      </Container>
+    );
+  }
+
+  renderImages() {
+    let images = [];
+    this.state.product.images.map((img, i) => {
+      images.push(
+          <TouchableWithoutFeedback
+            key={i}
+            onPress={() => this.openGallery(i)}
+          >
+            <Image
+              source={{uri: img}}
+              style={{width: Dimensions.get('window').width, height: 350}}
+              resizeMode="cover"
+            />
+          </TouchableWithoutFeedback>
+      );
+    });
+    return images;
+  }
+
+  renderColors() {
+    let colors = [];
+    this.state.product.colors.map((color, i) => {
+      colors.push(
+        <Item key={i} label={color} value={color} />
+      );
+    });
+    return colors;
+  }
+
+  renderSize() {
+    let size = [];
+    this.state.product.sizes.map((s, i) => {
+      size.push(
+        <Item key={i} label={s} value={s} />
+      );
+    });
+    return size;
+  }
+
+  price() {
+   var value = 0;
+    if (this.state.selectedSize == 'Single'){
+        value = this.state.quantity * 1.85;
+    }
+    if (this.state.selectedSize == 'Double'){
+        value = this.state.quantity * 2.20;
+    }
+    value = value.toFixed(2);
+    return value;
+  }
+
+
+  addToCart() {
+    var product = this.state.product;
+    var price = this.price();
+    product['size'] = this.state.selectedSize;
+    product['quantity'] = this.state.quantity;
+    product['price'] = price;
+    AsyncStorage.getItem("CART", (err, res) => {
+      if(!res) AsyncStorage.setItem("CART",JSON.stringify([product]));
+      else {
+        var items = JSON.parse(res);
+        items.push(product);
+        AsyncStorage.setItem("CART",JSON.stringify(items));
+      }
+    });
+  }
+
+
+}
+const styles = StyleSheet.create({
+image: {
+  alignItems: 'center',
+  height: 300,
+  width: 300,
+  marginTop: 40,
+  marginBottom: 20,
+  marginLeft: 40
+  },
+});
+const dummyProduct = {
+  id: 2,
+  title: 'Espresso',
+  xEspresso: 'empty',
+  xFlavor: 'empty',
+  milk: 'empty',
+  color: 'empty',
+  sizes: ['Single', 'Double',],
+};
   render() {
     var left = (
       <Left style={{flex:1}}>
